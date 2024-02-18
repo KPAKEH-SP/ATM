a7c7c230fec1f9ae5d74416e766ed2dee38491ce
Denis Emrih <den.emrikh@mail.ru>
2024-02-18 21:01:11 +0400

    Docker

---
+++ Dockerfile
@@ -0,0 +1,3 @@
+FROM openjdk:21-jdk
+COPY ./spring-atm1-spring-boot.jar spring-atm1.jar
+ENTRYPOINT ["java", "-jar", "spring-atm1.jar"]
--- src/main/Java/ru/denis/atm/repository/BanknoteRepository.java
+++
@@ -1,7 +0,0 @@
-package ru.denis.atm.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.denis.atm.Banknote;
-
-public interface BanknoteRepository extends JpaRepository<Banknote, Integer> {
-}
